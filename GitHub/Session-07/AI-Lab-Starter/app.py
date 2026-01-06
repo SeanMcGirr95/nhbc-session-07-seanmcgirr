db02b9470b8fd24c1c6f2f1ba847f7cfd842669e
Sean McGirr <smcgirr@nhbc.co.uk>
2026-01-06 13:45:23 +0000

    06012026 test save 3

--- GitHub/Session-07/AI-Lab-Starter/app.py
+++ GitHub/Session-07/AI-Lab-Starter/app.py
@@ -38,7 +38,6 @@ elif PROVIDER == 'azure':
     MODEL = "gpt-4o" # or your deployment name
 
 
-
 # TODO: Define your System Prompt here
 # TODO: Our example code assumes a JSON response; you can do something different if you want.
 SYSTEM_PROMPT = """
